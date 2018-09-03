9279cdab988e330a8d67c1f0722f265a1820dbc5
Leon Lu <aikqboy@gmail.com>
2018-09-02 18:50:48 -0700

    init commit

---
+++ Assets/PathfindingAI/Scripts/PlatformNode.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformNode : MonoBehaviour {
+
+    private List<PlatformNode> neibors;
+
+
+    public PlatformNode() {
+        neibors = new List<PlatformNode>();
+    }
+
+    public void AddNode(PlatformNode node) {
+        neibors.Add(node);
+    }
+
+    public List<PlatformNode> GetNearByNodes() {
+        return neibors;
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
